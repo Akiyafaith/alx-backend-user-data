073d91573b7412db09b87992955640bb177678db
Faith Nyaboke <akiyafaith@gmail.com>
2023-11-02 22:51:06 +0300

    personal

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 """a function called filter_datum"""
+import logging
 import re
 
 
@@ -7,3 +8,19 @@ def filter_datum(fields, redaction, message, separator):
     """returns the log message obfuscated"""
     regex = f'({separator}|^)({"|".join(fields)}=.+?)(?={separator}|$)'
     return re.sub(regex, f'\\1{redaction}', message)
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+    """
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+
+    def __init__(self, fields=None):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        if self.fields:
+            for field in self.fields:
+                record.msg = re.sub(f'{field}=.+?;', f'{field}={self.REDACTION};', record.msg)
+        return super(RedactingFormatter, self).format(record)
