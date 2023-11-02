8b8b36e7e049460c8d05348ba50e876b751808ff
Faith Nyaboke <akiyafaith@gmail.com>
2023-11-02 22:43:48 +0300

    personal data

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -5,5 +5,5 @@ import re
 
 def filter_datum(fields, redaction, message, separator):
     """returns the log message obfuscated"""
-    regex = f'(?<={separator}|^)({"|".join(fields)}=.+?)(?={separator}|$)'
-    return re.sub(regex, f'{redaction}', message)
+    regex = f'({separator}|^)({"|".join(fields)}=.+?)(?={separator}|$)'
+    return re.sub(regex, f'\\1{redaction}', message)
